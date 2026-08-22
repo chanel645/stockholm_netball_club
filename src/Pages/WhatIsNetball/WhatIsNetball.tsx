6e9d8131a9db6e8974218a32331bf9915af30eca
Chanel Sullivan <chanel645@gmail.com>
2026-08-22 19:49:28 +0200

    Positions

--- src/Pages/WhatIsNetball/WhatIsNetball.tsx
+++ src/Pages/WhatIsNetball/WhatIsNetball.tsx
@@ -15,15 +15,33 @@ export default function WhatIsNetball() {
   const isDesktopView = width > MobileWidth;
 
   const mainText = 'WHAT IS NETBALL?';
-  const historyTitle = 'The history of the game';
+  const historyTitle = 'The History of the Game';
   const historyText = (
     <>
       Netball derived from early versions of basketball and began in England in the 1890s. It is most popular in Commonwealth nations, specifically in schools, and is predominantly played by women. According to the INF, netball is played by more than 20 million people in more than 80 countries. In 1995, netball became an International Olympic Committee recognised sport, but it has not yet been played at the Olympic games. If you would like to see Netball at the 2032 Brisbane Olympics, please{' '}
           <a className='pledgeLink' href='https://netball.sport/back-the-bid/' target='_blank' rel='noreferrer'>sign the pledge here</a>.
     </>
   );
-  const rulesTitle = 'Rules Of Play';
-  const rulesText = 'Rules of Play Netball consists of 14 players allowed on court at any one time. A team may take the court with a minimum of 5 players and maximum of 7. This image shows the 7 positions needed on the court for one team.';
+  const rulesTitle = 'Rules of Play';
+  const rulesText = 'Netball consists of 14 players allowed on court at any one time. A team may take the court with a minimum of 5 players and maximum of 7. This image shows the 7 positions needed on the court for one team.';
+  const positionsText = (
+      <>
+        <br /><br />
+        <strong>Goal Keeper (GK):</strong> Work with the GD and to prevent the GS from scoring goals.
+        <br /><br />
+        <strong>Goal Defence (GD):</strong> Win the ball and reduce the effectiveness of the GA.
+        <br /><br />
+        <strong>Wing Defence (WD):</strong> Look for interceptions and to prevent the WA from feeding the circle.
+        <br /><br />
+        <strong>Center (C):</strong> Take the center pass and to link the defence and the attack.
+        <br /><br />
+        <strong>Wing Attack (WA):</strong> Feed the circle players giving them shooting opportunities.
+        <br /><br />
+        <strong>Goal Attack (GA):</strong> Feed balls to and work with GS and to score goals.
+        <br /><br />
+        <strong>Goal Shooter (GS):</strong> Score goals and to work in and around the circle with the GA.
+      </>
+    );
 
   if (isDesktopView)
   {
@@ -47,6 +65,9 @@ export default function WhatIsNetball() {
               <Text className='textLarge'>{rulesText}</Text>
             </div>
             <img className='positionsImage' src={positionsImage} alt='Netball positions and court markings' />
+            <div className='whatIsNetballTextDesktop'>
+              <Text className='textLarge'>{positionsText}</Text>
+            </div>
           </div>
         </div>
       </div>
@@ -74,6 +95,9 @@ export default function WhatIsNetball() {
               <Text className='textLarge'>{rulesText}</Text>
             </div>
             <img className='positionsImage' src={positionsImage} alt='Netball positions and court markings' />
+            <div className='whatIsNetballTextMobile'>
+              <Text className='textLarge'>{positionsText}</Text>
+            </div>
           </div>
           <WhatIsNetballMap/>
           <WhatIsNetballTableLong/>
